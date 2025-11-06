a5b134b963ed7ae1d5eb7ec3282b40e938cd41f6
wendy199703 <96010910+wendy199703@users.noreply.github.com>
2025-11-07 00:53:02 +0800

    新增yaml

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
+// ✅ 新增：讓 Render 能動態綁定埠口
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+app.Urls.Add($"http://0.0.0.0:{port}");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
